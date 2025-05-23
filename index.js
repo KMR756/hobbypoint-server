af7e3a9f772138b23761fdd8e357dae22646ae9b
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-05-24 02:28:20 +0600

    create API for get a specific data using id

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/groups/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await groupCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
