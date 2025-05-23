65e73ae4e7b23d675303c2d1749d456c0aae4916
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-05-24 02:07:12 +0600

    created deleting API for delete group

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -34,6 +34,13 @@ async function run() {
       const newGroup = req.body;
       console.log(newGroup);
       const result = await groupCollection.insertOne(newGroup);
+      res.send(result);
+    });
+
+    app.delete("/groups/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await groupCollection.deleteOne(query);
       res.send(result);
     });
 
